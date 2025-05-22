3163c4910da7a3116536fc7d4ef11f7b3ca52580
muceetech <65214908+muceetech@users.noreply.github.com>
2025-05-22 21:57:34 +0530

    Create KY016_RGB_LED.py

---
+++ Sensor_modules/KY016_RGB_LED.py
@@ -0,0 +1,24 @@
+import machine
+import time
+
+RED_PIN = machine.PWM(machine.Pin(25), freq=1000)
+GREEN_PIN = machine.PWM(machine.Pin(26), freq=1000)
+BLUE_PIN = machine.PWM(machine.Pin(27), freq=1000)
+
+def set_color(r, g, b):
+    RED_PIN.duty(r)
+    GREEN_PIN.duty(g)
+    BLUE_PIN.duty(b)
+
+while True:
+    set_color(1023, 0, 0)  # Red
+    time.sleep(1)
+    set_color(0, 1023, 0)  # Green
+    time.sleep(1)
+    set_color(0, 0, 1023)  # Blue
+    time.sleep(1)
+
+  
+
+  
+
