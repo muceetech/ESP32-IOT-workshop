7f9c5e7e493384ad8e71a3a6465f81d75b26f01d
muceetech <65214908+muceetech@users.noreply.github.com>
2025-05-22 21:38:48 +0530

    Update KY01_DB18S20_sensor.py

--- Sensor_modules/KY01_DB18S20_sensor.py
+++ Sensor_modules/KY01_DB18S20_sensor.py
@@ -1,1 +1,21 @@
+import machine
+import onewire
+import ds18x20
+import time
+
+pin = machine.Pin(4)
+ow = onewire.OneWire(pin)
+ds = ds18x20.DS18X20(ow)
+roms = ds.scan()
+while True:
+    ds.convert_temp()
+    time.sleep(1)
+    for rom in roms:
+        print("Temperature:", ds.read_temp(rom))
+    time.sleep(2)
+
+  
+
+  
+
 
